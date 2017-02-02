bfb65781099843246db1cabe499295c8aa25a158
sinedotwav <davidr@Aegis.local>
2017-02-01 19:34:57 -0800

    Personal Website
    My personal site of 2014.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// wow
+// sublime text 2 is really cool
+
+
+if ("highway" .length > "basketball" .length)
+{
+	confrim("the code");
+}
+
+else ("highway" .length < "basketball" .length)
+{
+	confrim("the person");
+}
+
+// wow
